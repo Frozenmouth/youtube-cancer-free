0d4a4b81d6c18ee0e12ec2e246ed76a508ad4307
Alexis Martin <alexismartin232@gmail.com>
2018-12-01 17:18:25 +0100

    Added NOT GotagaTV in the query parameter of list_searches

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     youtube_data_api = YoutubeDataApi.new.client
-    @result, @errors = youtube_data_api.list_searches 'snippet', q: 'gotaga'
+    @result, @errors = youtube_data_api.list_searches 'snippet', q: 'gotaga -GotagaTV'
   end
 end
