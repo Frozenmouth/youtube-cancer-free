b9c8f4e9632a1927732bc6bd492960a54b07cb38
Alexis Martin <alexismartin232@gmail.com>
2018-12-01 16:58:38 +0100

    replaced list_videos with list_searches

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     youtube_data_api = YoutubeDataApi.new.client
-    @result, @errors = youtube_data_api.list_videos 'snippet', id: 'qUeZgWqcTb4'
+    @result, @errors = youtube_data_api.list_searches 'snippet', q: 'gotaga'
   end
 end
