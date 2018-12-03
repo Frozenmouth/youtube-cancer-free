ed8ee000851fdd8b2c6770f2f294ce8960ed925e
Alexis Martin <alexismartin232@gmail.com>
2018-12-03 09:48:39 +0100

    added several cancer tags to avoid when researching something on YT

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     youtube_data_api = YoutubeDataApi.new.client
-    @result, @errors = youtube_data_api.list_searches 'snippet', q: 'gotaga -GotagaTV'
+    @result, @errors = youtube_data_api.list_searches 'snippet', q: 'trends france -cyprien -fortnite -gotaga -gamemixtreize -bodytime -farod -andy -telefoot -konbini -topito -inthepanda -vdbuzz -mickalow -sandrea'
   end
 end
