7e8f4386a86443a4fd652dbfd9e7f3986b17dd19
Alexis Martin <alexismartin232@gmail.com>
2018-12-03 10:19:29 +0100

    added maxresults = 50 parameter

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     youtube_data_api = YoutubeDataApi.new.client
-    @result, @errors = youtube_data_api.list_searches 'snippet', q: 'trends france -cyprien -fortnite -gotaga -gamemixtreize -bodytime -farod -andy -telefoot -konbini -topito -inthepanda -vdbuzz -mickalow -sandrea'
+    @result, @errors = youtube_data_api.list_searches 'snippet', q: 'trends france -cyprien -fortnite -gotaga -gamemixtreize -bodytime -farod -andy -telefoot -konbini -topito -inthepanda -vdbuzz -mickalow -sandrea', max_results: '50'
   end
 end
