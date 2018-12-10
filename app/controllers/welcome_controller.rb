8246c306794abac4d3d0756aca3a0342e61e24b1
Alexis Martin <alexismartin232@gmail.com>
2018-12-10 14:36:06 +0100

    Added <a href=""> tag

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,6 +7,6 @@ class WelcomeController < ApplicationController
 
   def index
     youtube_data_api = YoutubeDataApi.new.client
-    @result, @errors = youtube_data_api.list_videos 'snippet', chart: 'mostPopular', region_code:'FR', max_results: '50'
+    @result, @errors = youtube_data_api.list_videos 'snippet, id', chart: 'mostPopular', region_code: 'FR', max_results: '50'
   end
 end
