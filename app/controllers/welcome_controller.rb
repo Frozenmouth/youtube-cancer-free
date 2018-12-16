56197061517c2ad4eb7053593a8be51e0fe88f58
Alexis Martin <alexismartin232@gmail.com>
2018-12-16 19:14:41 +0100

    Added link to videos, removed query method, moved jquery import to layout.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
-  QUERY = <<-STRING
-  -jul
-  STRING
-
   def index
     youtube_data_api = YoutubeDataApi.new.client
-    @result, @errors = youtube_data_api.list_videos 'snippet, id', chart: 'mostPopular', region_code: 'FR', max_results: '50'
+    @result, @errors = youtube_data_api.list_videos 'snippet', chart: 'mostPopular', region_code: 'FR', max_results: 50
   end
 end
