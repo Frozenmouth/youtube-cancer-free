033fd197def8bb69ad99420efc4b9a989739ca93
Alexis Martin <alexismartin232@gmail.com>
2019-01-06 21:47:48 +0100

    Rejected channels

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,23 @@
 class WelcomeController < ApplicationController
   def index
     youtube_data_api = YoutubeDataApi.new
-    @result, @errors = youtube_data_api.trends('FR', 50)
+
+    next_page_token = nil
+    @videos = []
+    loop do
+      response = youtube_data_api.trends('FR', 50, next_page_token)
+      result, errors = response
+      raise StandardError, errors.to_s unless errors.nil?
+
+      next_page_token = response.next_page_token
+      trending_videos = result.items
+      @videos += trending_videos.reject do |video|
+        YoutubeDataApi::REJECTED_CHANNELS.any? do |rejected_channel|
+          video.snippet.channel_title.include? rejected_channel
+        end
+      end
+
+      break if next_page_token.nil?
+    end
   end
 end
--- app/services/youtube_data_api.rb
+++ app/services/youtube_data_api.rb
@@ -1,12 +1,22 @@
 # frozen_string_literal: true
 
 class YoutubeDataApi
+  REJECTED_CHANNELS = [
+    'Lacrim',
+    'EnjoyPhoenix',
+    'Bilal Hassani',
+    'VINSKY',
+    'Snapchat RED',
+    'INCROYABLE MAIS VRAI',
+    'Topito'
+  ].freeze
+
   def initialize
     @client = Google::Apis::YoutubeV3::YouTubeService.new
     @client.key = ENV['YOUTUBE_API_KEY']
   end
 
-  def trends(country, max_results)
-    @client.list_videos 'snippet', chart: 'mostPopular', region_code: country, max_results: max_results
+  def trends(country, max_results, page_token = nil)
+    @client.list_videos 'snippet', chart: 'mostPopular', region_code: country, max_results: max_results, page_token: page_token
   end
 end
