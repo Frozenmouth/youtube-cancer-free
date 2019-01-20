56babce4481c83839ccafe7553c7eab61382330d
Alexis <38560748+Frozenmouth@users.noreply.github.com>
2019-01-20 20:54:41 +0100

    Dropdown menu (#5)
    * Dropped 2 previous useless commits
    
    * Added country_select gem
    
    * Added flags emojis displaying alongside country names
    
    * Added dropdown menus with countries available on Youtube in it

--- app/services/youtube_data_api.rb
+++ app/services/youtube_data_api.rb
@@ -8,7 +8,8 @@ class YoutubeDataApi
     'VINSKY',
     'Snapchat RED',
     'INCROYABLE MAIS VRAI',
-    'Topito'
+    'Topito',
+    'Cyprien'
   ].freeze
 
   def initialize
@@ -18,5 +19,11 @@ class YoutubeDataApi
 
   def trends(country, max_results, page_token = nil)
     @client.list_videos 'snippet', chart: 'mostPopular', region_code: country, max_results: max_results, page_token: page_token
+  end
+
+  def countries
+    @client.list_i18n_regions('snippet', hl: 'en_US').items.map do |country|
+      [country.snippet.name, country.snippet.gl]
+    end
   end
 end
